87fdf91df8044de77b744a3b48a04a9f38b12bc6
yuchiwang <yuchiwang@163.com>
2016-10-28 12:17:04 +0800

    2016.10.28 setup

---
+++ facepp_ui_library/src/main/java/com/megvii/landmarklib/util/Util.java
@@ -0,0 +1,12 @@
+package com.megvii.landmarklib.util;
+
+/**
+ * Created by binghezhouke on 15-8-12.
+ */
+public class Util {
+	
+
+	public static String API_KEY = "TYg-TCe_rNVbjHumHLVKJOvDCagV6z-B";
+	public static String API_SECRET = "XiTsUF72sKOzK7hFbn6k4-5H8b0PNovF";
+	
+}
